3052314053b6053612b6cbca3003d063133933ab
eifrigmn <junwei0206@live.com>
2020-02-22 22:53:59 +0800

    203

---
+++ linked_list/0203_Remove_Elements/main.go
@@ -0,0 +1,26 @@
+package linked_list
+
+import "myleetcode/util"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode = util.ListNode
+
+func removeElements(head *ListNode, val int) *ListNode {
+	prv := &ListNode{}
+	prv.Next = head
+	l := prv
+	for prv.Next != nil {
+		if prv.Next.Val == val {
+			prv.Next = prv.Next.Next
+		} else {
+			prv = prv.Next
+		}
+	}
+	return l.Next
+}
