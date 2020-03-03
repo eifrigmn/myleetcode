c82e955e33f3be5f108bf9b074f2bbf3993ff6ef
eifrigmn <junwei0206@live.com>
2020-03-04 00:26:14 +0800

    350

---
+++ sort/0350_Insersection_of_Two_ArraysII/main.go
@@ -0,0 +1,50 @@
+package sort
+
+func intersect(nums1 []int, nums2 []int) []int {
+	if len(nums1) == 0 || len(nums2) == 0 {
+		return nil 
+	}
+	nums1 = quickSort(nums1)
+	nums2 = quickSort(nums2)
+	var p1, p2 int
+	var result []int
+	for p1 < len(nums1) && p2 < len(nums2) {
+		if nums1[p1] == nums2[p2] {
+			result = append(result, nums1[p1])
+			p1++
+			p2++
+		} else if nums1[p1] > nums2[p2] {
+			p2++
+		} else {
+			p1++
+		}
+	}
+	return result
+}
+
+func quickSort(nums []int) []int {
+	quickSortC(nums, 0, len(nums)-1)
+	return nums
+}
+
+func quickSortC(nums []int, lft, rgt int) {
+	if lft >= rgt {
+		return
+	}
+	p := partition(nums, lft, rgt)
+	quickSortC(nums, lft, p-1)
+	quickSortC(nums, p+1, rgt)
+}
+
+func partition(nums []int, lft, rgt int) int {
+	flag := lft
+	pivot := nums[rgt]
+	for i := lft; i < rgt; i++ {
+		if nums[i] < pivot {
+			nums[flag], nums[i] = nums[i], nums[flag]
+			flag++
+		}
+	}
+	nums[flag], nums[rgt] = nums[rgt], nums[flag]
+	return flag
+}
---
+++ sort/0350_Insersection_of_Two_ArraysII/main_test.go
@@ -0,0 +1,25 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestInsersect(t *testing.T) {
+	Convey("两个数组的交集", t, func() {
+		// nums1 = [1,2,2,1]
+		// nums2 = [2,2]
+		nums1 := []int{1, 2, 2, 1}
+		nums2 := []int{2, 2}
+		inums := intersect1(nums1, nums2)
+		So(reflect.DeepEqual(inums, []int{2, 2}), ShouldBeTrue)
+		// nums1 = [4,9,5]
+		// nums2 = [9,4,9,8,4]
+		nums1 = []int{4,9,5}
+		nums2 = []int{9,4,9,8,4}
+		inums = intersect1(nums1, nums2)
+		So(reflect.DeepEqual(inums, []int{4,9}), ShouldBeTrue)
+	})
+}
