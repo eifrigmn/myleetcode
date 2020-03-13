d5b5144f491aafdbe856f69fc2f08dc82a5ab955
eifrigmn <junwei0206@live.com>
2020-03-13 12:09:43 +0800

    234使用快慢指针法

--- linked_list/0234_Palindrome_Linked_List/main_test.go
+++ linked_list/0234_Palindrome_Linked_List/main_test.go
@@ -11,15 +11,15 @@ func TestIsPalindrome(t *testing.T) {
 	Convey("is palindrome", t, func(){
 		//1->2
 		l1 := util.IntsToSList([]int{1,2})
-		So(isPalindrome(l1), ShouldBeFalse)
+		So(isPalindrome2(l1), ShouldBeFalse)
 		// 1->2->2->1
 		l2 := util.IntsToSList([]int{1,2,2,1})
-		So(isPalindrome(l2), ShouldBeTrue)
+		So(isPalindrome2(l2), ShouldBeTrue)
 		// 空链表
 		l3 := util.IntsToSList([]int{})
-		So(isPalindrome(l3), ShouldBeTrue)
+		So(isPalindrome2(l3), ShouldBeTrue)
 		// 1->1->2->1
 		l4 := util.IntsToSList([]int{1,1,2,1})
-		So(isPalindrome(l4), ShouldBeFalse)
+		So(isPalindrome2(l4), ShouldBeFalse)
 	})
 }
