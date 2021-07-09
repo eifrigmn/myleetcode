b5c65d721d2119b9718f5e3ed98b182eaa46e61f
eifrigmn <junwei0206@live.com>
2021-07-09 16:33:09 +0800

    first bad version

---
+++ algorithm/primary/sort-search/02/main_test.go
@@ -0,0 +1,17 @@
+package _02
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFirstBadVersion(t *testing.T) {
+	Convey("第一个错误的版本", t, func() {
+		// n = 5, bad = 4
+		// 4
+		var n = 5
+		var bad = 4
+		// So(firstBadVersion())
+	})
+}
