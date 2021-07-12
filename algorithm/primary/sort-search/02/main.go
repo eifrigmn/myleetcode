c8107362f45ac16a68e02ced083fdbc4dc6ebedd
eifrigmn <junwei0206@live.com>
2021-07-12 17:13:39 +0800

    二分查找

--- algorithm/primary/sort-search/02/main.go
+++ algorithm/primary/sort-search/02/main.go
@@ -1,5 +1,7 @@
 package _02
 
+import "math"
+
 /**
  * Forward declaration of isBadVersion API.
  * @param   version   your guess about first bad version
@@ -7,7 +9,30 @@ package _02
  *			          false if current version is good
  * func isBadVersion(version int) bool;
  */
+var bad int
 
 func firstBadVersion(n int) int {
+	if isBadVersion(n) {
+		return findMid(1, n)
+	}
+	return findMid(n, math.MaxInt32)
+}
 
+func findMid(lft, rgt int) int {
+	mid := (lft + rgt) / 2
+	if mid == rgt || mid == lft {
+		if isBadVersion(lft) && isBadVersion(rgt) {
+			return lft
+		}
+		return rgt
+	}
+	if isBadVersion(mid) {
+		return findMid(lft, mid)
+	} else {
+		return findMid(mid, rgt)
+	}
+}
+
+func isBadVersion(version int) bool {
+	return version >= bad
 }
--- algorithm/primary/sort-search/02/main_test.go
+++ algorithm/primary/sort-search/02/main_test.go
@@ -9,9 +9,12 @@ import (
 func TestFirstBadVersion(t *testing.T) {
 	Convey("第一个错误的版本", t, func() {
 		// n = 5, bad = 4
-		// 4
 		var n = 5
-		var bad = 4
-		// So(firstBadVersion())
+		bad = 4
+		So(firstBadVersion(n), ShouldEqual, bad)
+		// n = 1, bad = 1
+		n = 1
+		bad = 1
+		So(firstBadVersion(n), ShouldEqual, bad)
 	})
 }
