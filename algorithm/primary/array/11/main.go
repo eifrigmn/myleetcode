daafdf50c882c0a3faee53aae0ea7ddbe1f3fa97
eifrigmn <junwei0206@live.com>
2021-07-29 11:33:51 +0800

    旋转矩阵

--- algorithm/primary/array/11/main.go
+++ algorithm/primary/array/11/main.go
@@ -1,18 +1,33 @@
 package _11
 
-func rotate(matrix [][]int)  {
+func rotate(matrix [][]int) {
 	l := len(matrix)
-	for i := 0;i<l;i++{
-		for j:=0;j<l/2;j++{
+	for i := 0; i < l; i++ {
+		for j := 0; j < l/2; j++ {
 			matrix[i][j], matrix[i][l-j-1] = matrix[i][l-j-1], matrix[i][j]
 		}
 	}
 
-	for i:=0;i<l-1;i++{
-		for j:=0;j<l-i-1;j++{
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-i-1; j++ {
 			matrix[i][j], matrix[l-1-j][l-1-i] = matrix[l-1-j][l-1-i], matrix[i][j]
 		}
 	}
 }
 
+func rotate1(matrix [][]int) {
+	// 对角线互换
+	l := len(matrix)
+	for i := 0; i < l; i++ {
+		for j := i; j < l; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
 
+	// 左右对称互换
+	for i := 0; i < l; i++ {
+		for j := 0; j < l/2; j++ {
+			matrix[i][j], matrix[i][l-j-1] = matrix[i][l-j-1], matrix[i][j]
+		}
+	}
+}
--- algorithm/primary/array/11/main_test.go
+++ algorithm/primary/array/11/main_test.go
@@ -1,28 +1,29 @@
 package _11
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
 	"reflect"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestRotate(t *testing.T) {
 	Convey("旋转图像", t, func() {
 		matrix := [][]int{
-			{1,2,3},
-			{4,5,6},
-			{7,8,9},
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
 		}
-		rotate(matrix)
-		So(reflect.DeepEqual(matrix, [][]int{{7,4,1},{8,5,2},{9,6,3}}), ShouldBeTrue)
+		rotate1(matrix)
+		So(reflect.DeepEqual(matrix, [][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}}), ShouldBeTrue)
 		matrix = [][]int{
-			{ 5, 1, 9,11},
-			{2, 4, 8,10},
+			{5, 1, 9, 11},
+			{2, 4, 8, 10},
 			{13, 3, 6, 7},
-			{15,14,12,16},
+			{15, 14, 12, 16},
 		}
-		rotate(matrix)
-		So(reflect.DeepEqual(matrix, [][]int{{15,13,2,5},{14,3,4,1},{12,6,8,9},{16,7,10,11}}), ShouldBeTrue)
+		rotate1(matrix)
+		So(reflect.DeepEqual(matrix, [][]int{{15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}}), ShouldBeTrue)
 	})
 
 }
